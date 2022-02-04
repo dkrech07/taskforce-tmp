bb32e05b384ed1f58fbc6d6a206f8369c7e12180
Дмитрий Кречетников <dkrech07@gmail.com>
2022-02-04 11:21:45 +0500

    Выводит страницу авторизации

---
+++ controllers/LandingController.php
@@ -0,0 +1,64 @@
+<?php
+
+namespace app\controllers;
+
+use Yii;
+use app\models\Tasks;
+use app\models\LoginForm;
+
+class LandingController extends \yii\web\Controller
+{
+
+    // Вход на сайт
+    // public function actionLogin()
+    // {
+    //     $loginForm = new LoginForm();
+    //     if (\Yii::$app->request->getIsPost()) {
+    //         $loginForm->load(\Yii::$app->request->post());
+    //         if ($loginForm->validate()) {
+    //             $user = $loginForm->getUser();
+    //             \Yii::$app->user->login($user);
+    //             return $this->goHome();
+    //         }
+    //     }
+    // }
+
+
+    public $layout = 'landing';
+
+    // public function beforeAction($action)
+    // {
+    //     parent::beforeAction($action);
+    //     if (Yii::$app->user->getId()) {
+    //         $this->redirect('/tasks/index');
+    //     }
+    //     return true;
+    // }
+
+    // Отрисовка страницы авторизации, вывод формы авторизации;
+    public function actionIndex()
+    {
+        // $loginForm = [];
+        // $tasks = Tasks::getLastTasks(4);
+
+        $loginForm = new LoginForm();
+
+        return $this->render('index', ['loginForm' => $loginForm]);
+    }
+
+    // Вход на сайт, переход на страницу задач;
+    public function actionLogin()
+    {
+        $loginForm = new LoginForm();
+        if (\Yii::$app->request->getIsPost()) {
+            $loginForm->load(\Yii::$app->request->post());
+            if ($loginForm->validate()) {
+                $user = $loginForm->getUser();
+                \Yii::$app->user->login($user);
+                // return $this->goHome();
+
+                $this->redirect('landing');
+            }
+        }
+    }
+}
