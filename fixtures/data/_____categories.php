efc3384c0505e0aa3109d4fc290e07afa2fecbfe
Дмитрий <dkrech07@gmail.com>
2022-03-05 11:31:16 +0500

    Прогружает тестовые данные для городов и категорий

--- fixtures/data/_____categories.php
+++ fixtures/data/_____categories.php
@@ -2,35 +2,43 @@
 
 return [
     'categories0' => [
-        'name' => 'Переводы',
+        'name' => 'Loan Interviewer',
         'icon' => 'C:\\xampp\\tmp\\4b7c9df6321b8d5089be311db9026763.png',
     ],
     'categories1' => [
-        'name' => 'Уборка',
+        'name' => 'Construction',
         'icon' => 'C:\\xampp\\tmp\\5e249807531c3fec1be25c912d26213f.png',
     ],
     'categories2' => [
-        'name' => 'Переезды',
+        'name' => 'Mail Machine Operator',
         'icon' => 'C:\\xampp\\tmp\\02cb3b8b3fdfdcb759e310b3e29360dc.png',
     ],
     'categories3' => [
-        'name' => 'Компьютерная помощь',
+        'name' => 'Plant Scientist',
         'icon' => 'C:\\xampp\\tmp\\736a4f21f38f0fa9f2aa4ab9b4725986.png',
     ],
     'categories4' => [
-        'name' => 'Ремонт квартирный',
+        'name' => 'Electronics Engineering Technician',
         'icon' => 'C:\\xampp\\tmp\\54c8b5a179b3ecf7ecbf2b78ada5f6c9.png',
     ],
     'categories5' => [
-        'name' => 'Ремонт техники',
+        'name' => 'Computer Software Engineer',
         'icon' => 'C:\\xampp\\tmp\\33ebdc92b91a2ab4dc6c21af15c7148a.png',
     ],
     'categories6' => [
-        'name' => 'Красота',
+        'name' => 'Tile Setter OR Marble Setter',
         'icon' => 'C:\\xampp\\tmp\\abaaad6880899729cd749ee6af7f927b.png',
     ],
     'categories7' => [
-        'name' => 'Фото',
+        'name' => 'Building Inspector',
         'icon' => 'C:\\xampp\\tmp\\7d7c98b686b45affa6d525307df7b1b9.png',
+    ],
+    'categories8' => [
+        'name' => 'Forestry Conservation Science Teacher',
+        'icon' => 'C:\\xampp\\tmp\\2744952a7e5f645dc1a3acd5aa46c807.png',
+    ],
+    'categories9' => [
+        'name' => 'Office Clerk',
+        'icon' => 'C:\\xampp\\tmp\\51604f1e119104c23151296f4ba3d91e.png',
     ],
 ];
---
+++ fixtures/data/_____cities.php
@@ -0,0 +1,54 @@
+<?php
+
+return [
+    'cities0' => [
+        'city' => 'Grantshire',
+        'latitude' => 87.09981787,
+        'longitude' => -106.83630642,
+    ],
+    'cities1' => [
+        'city' => 'Champlinfurt',
+        'latitude' => 2.17404608,
+        'longitude' => -62.30380822,
+    ],
+    'cities2' => [
+        'city' => 'North Americoside',
+        'latitude' => 21.62411971,
+        'longitude' => -100.02553919,
+    ],
+    'cities3' => [
+        'city' => 'Kallieton',
+        'latitude' => 15.08425371,
+        'longitude' => -74.99018446,
+    ],
+    'cities4' => [
+        'city' => 'Casperfurt',
+        'latitude' => 46.78066788,
+        'longitude' => 21.50241481,
+    ],
+    'cities5' => [
+        'city' => 'South Creola',
+        'latitude' => 26.55003265,
+        'longitude' => 12.88136286,
+    ],
+    'cities6' => [
+        'city' => 'Jabariberg',
+        'latitude' => 70.4625167,
+        'longitude' => 53.27370817,
+    ],
+    'cities7' => [
+        'city' => 'East Hilmaton',
+        'latitude' => 80.84095371,
+        'longitude' => 33.4665352,
+    ],
+    'cities8' => [
+        'city' => 'South Urbanberg',
+        'latitude' => 43.51810858,
+        'longitude' => -60.80732552,
+    ],
+    'cities9' => [
+        'city' => 'New Abraham',
+        'latitude' => 15.69153374,
+        'longitude' => -109.25354554,
+    ],
+];
